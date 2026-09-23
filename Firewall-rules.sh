f23dea37531efd9f5900325c0a919c4859f4508d
Mohammed Abrar Khan <mohammedabrarkhan18@gmail.com>
2026-09-23 17:44:22 +0530

    Update firewall rules with specific IP addresses

--- Firewall-rules.sh
+++ Firewall-rules.sh
@@ -3,9 +3,9 @@ sudo iptables -L
 # rule to drop icmp request pacKets
 sudo iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 # rule to block incoming SSH connection
-sudo iptables -A INPUT -p tcp --dport 22 -s clientIP -j DROP
+sudo iptables -A INPUT -p tcp --dport 22 -s 192.169.0.121 -j DROP
 # rule to block a website
-sudo iptables -A OUTPUT -p tcp -d websiteIP -j REJECT
+sudo iptables -A OUTPUT -p tcp -d 142.250.146.113 -j REJECT
 # rule to block incoming TCP packets from the specified IP address to port 80 (HTTP)
 sudo iptables -A INPUT -p tcp --dport 80 -s 192.168.0.50 -j DROP
 # Deleting rules
